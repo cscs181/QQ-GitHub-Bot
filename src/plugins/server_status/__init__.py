c1c2935a580e1e4c6d5e1004cfa4d27284901896
yanyongyu <yanyongyu_1@126.com>
2020-11-13 02:04:28 +0800

    :children_crossing: improve event handle

--- src/plugins/server_status/__init__.py
+++ src/plugins/server_status/__init__.py
@@ -4,7 +4,7 @@
 @Author         : yanyongyu
 @Date           : 2020-09-18 00:00:13
 @LastEditors    : yanyongyu
-@LastEditTime   : 2020-10-04 16:39:15
+@LastEditTime   : 2020-10-07 01:10:33
 @Description    : None
 @GitHub         : https://github.com/yanyongyu
 """
@@ -12,12 +12,13 @@ __author__ = "yanyongyu"
 
 from nonebot.typing import Bot, Event
 from nonebot.permission import SUPERUSER
-from nonebot import get_driver, on_command, on_notice
+from nonebot import get_driver, on_command, on_notice, on_message
 
 from .config import Config
 from .data_source import cpu_status, memory_status, disk_usage
 
-status_config = Config(**get_driver().config.dict())
+global_config = get_driver().config
+status_config = Config(**global_config.dict())
 
 command = on_command("状态", permission=SUPERUSER, priority=10)
 
@@ -39,10 +40,20 @@ async def server_status(bot: Bot, event: Event, state: dict):
     await bot.send(message="\n".join(data), event=event)
 
 
-async def _poke(bot: Bot, event: Event, state: dict) -> bool:
+async def _group_poke(bot: Bot, event: Event, state: dict) -> bool:
     return (event.detail_type == "notify" and event.sub_type == "poke" and
-            str(event.raw_event["target_id"]) == bot.self_id)
+            str(event.raw_event["target_id"]) == bot.self_id and
+            event.user_id in global_config.superusers)
 
 
-poke = on_notice(_poke, priority=10, block=True)
+group_poke = on_notice(_group_poke, priority=10, block=True)
+group_poke.handle()(server_status)
+
+
+async def _poke(bot: Bot, event: Event, state: dict) -> bool:
+    return (event.detail_type == "private" and event.sub_type == "friend" and
+            event.message[0].type == "poke")
+
+
+poke = on_message(_poke, permission=SUPERUSER, priority=10)
 poke.handle()(server_status)
