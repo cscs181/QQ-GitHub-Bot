b3729df8b5dbcba2470ac1bc75ed9fcef5906f75
yanyongyu <yanyongyu_1@126.com>
2020-11-17 17:14:48 +0800

    :art: improve sub plugin structure

--- src/plugins/github/__init__.py
+++ src/plugins/github/__init__.py
@@ -4,7 +4,7 @@
 @Author         : yanyongyu
 @Date           : 2020-09-20 23:59:20
 @LastEditors    : yanyongyu
-@LastEditTime   : 2020-10-04 15:13:26
+@LastEditTime   : 2020-11-17 17:14:09
 @Description    : GitHub Main Plugin
 @GitHub         : https://github.com/yanyongyu
 """
@@ -21,4 +21,5 @@ _sub_plugins = set()
 # load all github plugin config from global config
 github_config = Config(**nonebot.get_driver().config.dict())
 
-_sub_plugins |= nonebot.load_plugins(str(Path(__file__).parent.resolve()))
+_sub_plugins |= nonebot.load_plugins(
+    str((Path(__file__).parent / "plugins").resolve()))
