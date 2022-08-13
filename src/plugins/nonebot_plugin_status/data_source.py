9075dbb07099a309d9d2a30bb79ef68d27800806
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2022-08-13 08:21:45 +0000

    :bug: suppress exception when get disk usage

--- src/plugins/nonebot_plugin_status/data_source.py
+++ src/plugins/nonebot_plugin_status/data_source.py
@@ -4,17 +4,18 @@
 @Author         : yanyongyu
 @Date           : 2020-09-18 00:15:21
 @LastEditors    : yanyongyu
-@LastEditTime   : 2022-05-23 04:38:37
+@LastEditTime   : 2022-08-13 08:20:43
 @Description    : None
 @GitHub         : https://github.com/yanyongyu
 """
 __author__ = "yanyongyu"
 
 import time
-from typing import Dict, List
 from datetime import timedelta
+from typing import Dict, List, Optional
 
 import psutil
+from nonebot.log import logger
 
 
 def cpu_status() -> float:
@@ -29,9 +30,20 @@ def memory_status() -> float:
     return psutil.virtual_memory().percent
 
 
+def _get_disk_usage(path: str) -> Optional[psutil._common.sdiskusage]:
+    try:
+        return psutil.disk_usage(path)
+    except Exception as e:
+        logger.warning(f"Could not get disk usage for {path}: {e!r}")
+
+
 def disk_usage() -> Dict[str, psutil._common.sdiskusage]:
     disk_parts = psutil.disk_partitions()
-    return {d.mountpoint: psutil.disk_usage(d.mountpoint) for d in disk_parts}
+    return {
+        d.mountpoint: usage
+        for d in disk_parts
+        if (usage := _get_disk_usage(d.mountpoint))
+    }
 
 
 def uptime() -> timedelta:
