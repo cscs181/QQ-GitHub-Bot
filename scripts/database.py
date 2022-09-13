096f2221d1a2c5b427f649f6c972872eea873429
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2022-09-13 15:52:44 +0000

    :sparkles: add database init

--- scripts/database.py
+++ scripts/database.py
@@ -1,10 +1,13 @@
 import sys
 import asyncio
 from pathlib import Path
+from functools import partial
 from argparse import ArgumentParser
 
 from aerich import Command
+from nonebot.log import logger
 
+logger.remove()
 root = Path(__file__).parent.parent
 
 sys.path.append(str(root))
@@ -17,8 +20,8 @@ cmd = Command(tortoise_config, app="bot", location=str(root / "migrations"))
 parser = ArgumentParser()
 sub_parser = parser.add_subparsers(required=True)
 
-init = sub_parser.add_parser("init")
-init.set_defaults(func=cmd.init)
+init_db = sub_parser.add_parser("init-db")
+init_db.set_defaults(func=partial(cmd.init_db, safe=True))
 
 migrate = sub_parser.add_parser("migrate")
 migrate.set_defaults(func=cmd.migrate)
@@ -29,9 +32,20 @@ upgrade.set_defaults(func=cmd.upgrade)
 downgrade = sub_parser.add_parser("downgrade")
 downgrade.set_defaults(func=cmd.downgrade)
 
+history = sub_parser.add_parser("history")
+history.set_defaults(func=cmd.history)
 
-if __name__ == "__main__":
+heads = sub_parser.add_parser("heads")
+heads.set_defaults(func=cmd.heads)
+
+
+async def main():
     result = parser.parse_args()
     result = vars(result)
     command = result.pop("func")
-    asyncio.run(command())
+    await cmd.init()
+    await command()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
