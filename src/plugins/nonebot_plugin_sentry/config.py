ee180737faf3caf201deae1c13750ae05c011921
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-06-17 10:46:14 +0800

    :bug: fix sentry plugin typing

--- src/plugins/nonebot_plugin_sentry/config.py
+++ src/plugins/nonebot_plugin_sentry/config.py
@@ -4,13 +4,13 @@
 @Author         : yanyongyu
 @Date           : 2020-11-23 18:44:18
 @LastEditors    : yanyongyu
-@LastEditTime   : 2023-03-30 19:56:41
+@LastEditTime   : 2023-06-17 10:45:33
 @Description    : Config for Sentry plugin
 @GitHub         : https://github.com/yanyongyu
 """
 __author__ = "yanyongyu"
 
-from typing import Any, Optional
+from typing import Any, Dict, Optional
 
 from nonebot.log import logger
 from pydantic import Extra, BaseModel, validator, root_validator
@@ -24,7 +24,7 @@ class Config(BaseModel, extra=Extra.allow):
     sentry_default_integrations: bool = False
 
     @root_validator(pre=True)
-    def filter_sentry_configs(cls, values: dict[str, Any]):
+    def filter_sentry_configs(cls, values: Dict[str, Any]):
         return {
             key: value for key, value in values.items() if key.startswith("sentry_")
         }
