8cbe79c93b45776b7ead6f5ac6a9e242607556be
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-11-07 15:17:36 +0800

    :heavy_minus_sign: remove local sentry plugin

---
+++ src/providers/sentry/__init__.py
@@ -0,0 +1,14 @@
+"""
+@Author         : yanyongyu
+@Date           : 2023-11-07 15:16:04
+@LastEditors    : yanyongyu
+@LastEditTime   : 2023-11-07 15:16:04
+@Description    : None
+@GitHub         : https://github.com/yanyongyu
+"""
+
+__author__ = "yanyongyu"
+
+from nonebot import require
+
+require("nonebot_plugin_sentry")
