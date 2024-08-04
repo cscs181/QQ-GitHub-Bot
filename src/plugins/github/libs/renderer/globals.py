b6684460c83c9a62dd04f4516537575a2761e26b
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-08-04 14:11:21 +0800

    :bug: fix render style

--- src/plugins/github/libs/renderer/globals.py
+++ src/plugins/github/libs/renderer/globals.py
@@ -2,7 +2,7 @@
 @Author         : yanyongyu
 @Date           : 2022-09-14 16:09:04
 @LastEditors    : yanyongyu
-@LastEditTime   : 2024-05-15 23:31:57
+@LastEditTime   : 2024-08-04 14:05:47
 @Description    : Jinjia globals for renderer
 @GitHub         : https://github.com/yanyongyu
 """
@@ -15,4 +15,4 @@ def scale_linear(value: int, width: int, changed: int) -> int:
 
     See https://github.com/git/git/blob/bcd6bc478adc4951d57ec597c44b12ee74bc88fb/diff.c#L2500-L2511.
     """
-    return 1 + int(value * (width - 1) / changed) if value else value
+    return value and (1 + int(value * (width - 1) / changed))
